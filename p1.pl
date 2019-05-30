c77c5653fc172242133f8533920ea9c7c68f70c1
Soledad Leiva <soledadleiva@MacBook-de-Soledad.local>
2019-05-30 01:21:32 -0300

    punto 1

---
+++ p1.pl
@@ -0,0 +1,31 @@
+% PUNTO 1 
+candidato(frank, 50, rojo).
+candidato(claire, 52, rojo).
+candidato(garrett, 64, azul).
+candidato(peter, 26, _). 
+candidato(jackie, 38, amarillo).
+candidato(linda, 30, azul).
+candidato(catherine, 59, rojo).
+candidato(seth, _, amarillo).
+candidato(heather, 50, amarillo). 
+
+partido(buenosAires, azul, 15355000).
+partido(buenosAires, rojo, 15355000).
+partido(buenosAires, amarillo, 15355000).
+partido(chaco, azul, 1143201).
+partido(chaco, amarillo, 1143201).
+partido(tierraDelFuego, azul, 160720).
+partido(tierraDelFuego, rojo, 160720).
+partido(sanLuis, azul, 489255).
+partido(sanLuis, rojo, 489255).
+partido(neuquen, azul, 637913).
+partido(santaFe,rojo, 3397532).
+partido(cordoba, rojo, 3567654).
+partido(chubut, rojo, 577466).
+partido(formosa, amarillo, 527895).
+partido(tucuman, amarillo, 1687305).
+partido(salta, amarillo, 1333365).
+partido(santaCruz, amarillo, 273964).
+partido(laPampa, amarillo, 349299).
+partido(corrientes, amarillo, 992595).
+partido(misiones, amarillo, 1189446).
