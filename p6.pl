c64ec4a3892a25bd33394ebb6c76fb1f91e932f2
Soledad Leiva <soledadleiva@MacBook-de-Soledad.local>
2019-06-10 01:07:19 -0300

    punto 6

---
+++ p6.pl
@@ -0,0 +1,17 @@
+% PUNTO 6
+%PromesasAzul
+promete(azul,edificio(hospitales,1000)).
+promete(azul,edificio(jardines,100)).
+promete(azul,edificio(escuelas,5)).
+promete(azul,inflacion(2,4)).
+promete(azul,construir(edificio(hospitales,1000), edificio(jardines,100), edificio(escuelas,5))).
+%PromesasAmarillo
+promete(amarillo,edificio(hospitales,100)).
+promete(amarillo,edificio(universidades,1)).
+promete(amarillo,edificio(comiseria,200)).
+promete(amarillo,nuevosPuestosDeTrabajo(10000)).
+promete(amarillo,inflacion(1,15)).
+promete(amarillo,construir(edificio(hospitales,100), edificio(universidades,1), edificio(comiseria,200))).
+%PromesasRojo
+promete(rojo,nuevosPuestosDeTrabajo(800000)).
+promete(azul,inflacion(10,30)).
